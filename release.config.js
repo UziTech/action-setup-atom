85285a8ec6544ea11990455535e4df0034a9dfee
Tony Brix <tony@brix.ninja>
2019-11-15 23:31:41 -0600

    chore(ci): add semantic-release and husky

---
+++ release.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	"plugins": [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/changelog",
+		["@semantic-release/npm", {
+			"npmPublish": false,
+		}],
+		"@semantic-release/github",
+		"@semantic-release/git",
+	],
+};
