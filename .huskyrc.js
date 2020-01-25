1d80ad63a26017082f219e9034f8b9496cb574d3
Tony Brix <tony@brix.ninja>
2020-01-24 19:42:03 -0600

    chore: check dist staged in pre-commit

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,5 +1,6 @@
 module.exports = {
 	"hooks": {
+		"pre-commit": "git diff --staged --quiet -- dist || (echo \"Please don't commit the 'dist' folder\" && exit 1)",
 		"commit-msg": "commitlint -E HUSKY_GIT_PARAMS",
 	}
 };
