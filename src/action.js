8aeb087043f88ce52ea72ade9c4e3c0c6b0ecdb3
Tony Brix <tony@brix.ninja>
2020-01-24 23:03:58 -0600

    feat: add npm package `setup-atom`

--- src/action.js
+++ src/action.js
@@ -1,15 +1,35 @@
+#!/usr/bin/env node
+
 const path = require("path");
+if (!process.env.GITHUB_ACTIONS) {
+	if (process.env.USERPROFILE) {
+		process.env.RUNNER_TEMP = path.resolve(process.env.USERPROFILE, "./temp");
+	} else if (process.env.HOME) {
+		process.env.RUNNER_TEMP = path.resolve(process.env.HOME, "./temp");
+	} else {
+		process.env.RUNNER_TEMP = path.resolve("../temp");
+	}
+}
 const core = require("@actions/core");
-const downloadAtom = require("./download.js");
+const {downloadAtom, addToPath} = require("./setup-atom.js");
 
 async function run() {
 	try {
-		const channel = core.getInput("channel", {required: true}).toLowerCase();
-		const folder = path.join(process.env.GITHUB_WORKSPACE, "atom");
-		const paths = await downloadAtom(channel, folder);
-		paths.forEach(p => core.addPath(p));
+		const channel = (process.env.GITHUB_ACTIONS && core.getInput("channel").toLowerCase()) || process.argv[2] || "stable";
+		const folder = path.resolve(process.env.RUNNER_TEMP, process.argv[3] || "./atom");
+		console.log("channel:", channel);
+		console.log("folder:", folder);
+
+		await downloadAtom(channel, folder);
+		await addToPath(channel, folder);
+
 	} catch (error) {
-		core.setFailed(error.message);
+		if (process.env.GITHUB_ACTIONS) {
+			core.setFailed(error.message);
+		} else {
+			console.error(error);
+			process.exit(1);
+		}
 	}
 }
 
