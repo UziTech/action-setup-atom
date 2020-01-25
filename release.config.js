5a56e742b1b5458948aefe1e3564fc94a302cbb8
Tony Brix <tony@brix.ninja>
2020-01-24 19:46:46 -0600

    chore(ci): publish to npm

--- release.config.js
+++ release.config.js
@@ -3,9 +3,7 @@ module.exports = {
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
 		"@semantic-release/changelog",
-		["@semantic-release/npm", {
-			"npmPublish": false,
-		}],
+		"@semantic-release/npm",
 		"@semantic-release/github",
 		"@semantic-release/git",
 	],
