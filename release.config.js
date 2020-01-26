779e76ea821fb61fb03351ce772e70fa4f681dab
Tony Brix <tony@brix.ninja>
2020-01-25 23:06:15 -0600

    chore(ci): re-add build step

--- release.config.js
+++ release.config.js
@@ -3,9 +3,6 @@ module.exports = {
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
 		"@semantic-release/changelog",
-		["@semantic-release/exec", {
-			"prepareCmd": "npm run build"
-		}],
 		"@semantic-release/npm",
 		"@semantic-release/github",
 		"@semantic-release/git",
