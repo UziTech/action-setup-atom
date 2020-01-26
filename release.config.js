74eee7304a28471f7ae258bcb6051ffe438af69d
Tony Brix <tony@brix.ninja>
2020-01-25 21:34:04 -0600

    chore(ci): build in release

--- release.config.js
+++ release.config.js
@@ -3,6 +3,9 @@ module.exports = {
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
 		"@semantic-release/changelog",
+		["@semantic-release/exec", {
+			"prepareCmd": "npm run build"
+		}],
 		"@semantic-release/npm",
 		"@semantic-release/github",
 		"@semantic-release/git",
