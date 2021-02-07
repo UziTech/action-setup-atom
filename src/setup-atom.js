8ea351bb5d4be38787f76044a88d400636aa658d
Restyled.io <commits@restyled.io>
2021-02-07 05:23:28 +0000

    Restyled by prettier

--- src/setup-atom.js
+++ src/setup-atom.js
@@ -10,8 +10,8 @@ if (!process.env.GITHUB_ACTIONS) {
 }
 const tc = require("@actions/tool-cache");
 const core = require("@actions/core");
-const {exec} = require("@actions/exec");
-const {promisify} = require("util");
+const { exec } = require("@actions/exec");
+const { promisify } = require("util");
 const cp = require("child_process");
 const execAsync = promisify(cp.exec);
 const fs = require("fs");
@@ -25,108 +25,121 @@ async function downloadAtom(channel, folder) {
     folder = path.resolve(process.env.RUNNER_TEMP, "./atom");
   }
   switch (process.platform) {
-  case "win32": {
-    const downloadFile = await tc.downloadTool(
-        `https://atom.io/download/windows_zip?channel=${channel}`);
-    await tc.extractZip(downloadFile, folder);
-    break;
-  }
-  case "darwin": {
-    const downloadFile = await tc.downloadTool(
-        `https://atom.io/download/mac?channel=${channel}`);
-    await tc.extractZip(downloadFile, folder);
-    break;
-  }
-  default: {
-    const downloadFile = await tc.downloadTool(
-        `https://atom.io/download/deb?channel=${channel}`);
-    await exec("sudo apt-get update");
-    await exec(
-        "sudo apt-get install -yyq libgconf-2-4 build-essential git libsecret-1-dev gvfs-bin");
-    await exec("dpkg-deb", [ "-x", downloadFile, folder ]);
-    break;
-  }
+    case "win32": {
+      const downloadFile = await tc.downloadTool(
+        `https://atom.io/download/windows_zip?channel=${channel}`
+      );
+      await tc.extractZip(downloadFile, folder);
+      break;
+    }
+    case "darwin": {
+      const downloadFile = await tc.downloadTool(
+        `https://atom.io/download/mac?channel=${channel}`
+      );
+      await tc.extractZip(downloadFile, folder);
+      break;
+    }
+    default: {
+      const downloadFile = await tc.downloadTool(
+        `https://atom.io/download/deb?channel=${channel}`
+      );
+      await exec("sudo apt-get update");
+      await exec(
+        "sudo apt-get install -yyq libgconf-2-4 build-essential git libsecret-1-dev gvfs-bin"
+      );
+      await exec("dpkg-deb", ["-x", downloadFile, folder]);
+      break;
+    }
   }
 }
 
 async function addToPath(channel, folder) {
   switch (process.platform) {
-  case "win32": {
-    let atomfolder = "Atom";
-    if (channel !== "stable") {
-      atomfolder += ` ${channel[0].toUpperCase() + channel.substring(1)}`;
+    case "win32": {
+      let atomfolder = "Atom";
+      if (channel !== "stable") {
+        atomfolder += ` ${channel[0].toUpperCase() + channel.substring(1)}`;
+      }
+      const atomPath = path.join(folder, atomfolder, "resources", "cli");
+      if (process.env.GITHUB_ACTIONS) {
+        core.addPath(atomPath);
+      } else {
+        await exec("powershell", [
+          "-Command",
+          [
+            `[Environment]::SetEnvironmentVariable("PATH", "${atomPath};" + $env:PATH, "Machine")`,
+            "Start-Sleep -s 10",
+            "Restart-Computer",
+            "Start-Sleep -s 10",
+          ].join(";\n"),
+        ]);
+      }
+      break;
     }
-    const atomPath = path.join(folder, atomfolder, "resources", "cli");
-    if (process.env.GITHUB_ACTIONS) {
-      core.addPath(atomPath);
-    } else {
-      await exec("powershell", [
-        "-Command",
-        [
-          `[Environment]::SetEnvironmentVariable("PATH", "${
-              atomPath};" + $env:PATH, "Machine")`,
-          "Start-Sleep -s 10",
-          "Restart-Computer",
-          "Start-Sleep -s 10",
-        ].join(";\n")
+    case "darwin": {
+      let atomfolder = "Atom";
+      if (channel !== "stable") {
+        atomfolder += ` ${channel[0].toUpperCase() + channel.substring(1)}`;
+      }
+      atomfolder += ".app";
+      const atomPath = path.join(
+        folder,
+        atomfolder,
+        "Contents",
+        "Resources",
+        "app"
+      );
+      await exec("ln", [
+        "-s",
+        path.join(atomPath, "atom.sh"),
+        path.join(atomPath, "atom"),
       ]);
+      const apmPath = path.join(atomPath, "apm", "bin");
+      if (process.env.GITHUB_ACTIONS) {
+        core.addPath(atomPath);
+        core.addPath(apmPath);
+      } else {
+        await execAsync(`export "PATH=${atomPath}:${apmPath}:$PATH"`);
+        await writeFileAsync(
+          "../env.sh",
+          ["#! /bin/bash", `export "PATH=${atomPath}:${apmPath}:$PATH"`].join(
+            "\n"
+          ),
+          { mode: "777" }
+        );
+      }
+      break;
     }
-    break;
-  }
-  case "darwin": {
-    let atomfolder = "Atom";
-    if (channel !== "stable") {
-      atomfolder += ` ${channel[0].toUpperCase() + channel.substring(1)}`;
+    default: {
+      const display = ":99";
+      await exec(
+        `/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- ${display} -ac -screen 0 1280x1024x16 +extension RANDR`
+      );
+      let atomfolder = "atom";
+      if (channel !== "stable") {
+        atomfolder += `-${channel}`;
+      }
+      const atomPath = path.join(folder, "usr", "share", atomfolder);
+      const apmPath = path.join(atomPath, "resources", "app", "apm", "bin");
+      if (process.env.GITHUB_ACTIONS) {
+        await core.exportVariable("DISPLAY", display);
+        core.addPath(atomPath);
+        core.addPath(apmPath);
+      } else {
+        await execAsync(`export DISPLAY="${display}"`);
+        await execAsync(`export "PATH=${atomPath}:${apmPath}:$PATH"`);
+        await writeFileAsync(
+          "../env.sh",
+          [
+            "#! /bin/bash",
+            `export DISPLAY="${display}"`,
+            `export "PATH=${atomPath}:${apmPath}:$PATH"`,
+          ].join("\n"),
+          { mode: "777" }
+        );
+      }
+      break;
     }
-    atomfolder += ".app";
-    const atomPath =
-        path.join(folder, atomfolder, "Contents", "Resources", "app");
-    await exec(
-        "ln",
-        [ "-s", path.join(atomPath, "atom.sh"), path.join(atomPath, "atom") ]);
-    const apmPath = path.join(atomPath, "apm", "bin");
-    if (process.env.GITHUB_ACTIONS) {
-      core.addPath(atomPath);
-      core.addPath(apmPath);
-    } else {
-      await execAsync(`export "PATH=${atomPath}:${apmPath}:$PATH"`);
-      await writeFileAsync("../env.sh",
-                           [
-                             "#! /bin/bash",
-                             `export "PATH=${atomPath}:${apmPath}:$PATH"`,
-                           ].join("\n"),
-                           {mode : "777"});
-    }
-    break;
-  }
-  default: {
-    const display = ":99";
-    await exec(
-        `/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- ${
-            display} -ac -screen 0 1280x1024x16 +extension RANDR`);
-    let atomfolder = "atom";
-    if (channel !== "stable") {
-      atomfolder += `-${channel}`;
-    }
-    const atomPath = path.join(folder, "usr", "share", atomfolder);
-    const apmPath = path.join(atomPath, "resources", "app", "apm", "bin");
-    if (process.env.GITHUB_ACTIONS) {
-      await core.exportVariable("DISPLAY", display);
-      core.addPath(atomPath);
-      core.addPath(apmPath);
-    } else {
-      await execAsync(`export DISPLAY="${display}"`);
-      await execAsync(`export "PATH=${atomPath}:${apmPath}:$PATH"`);
-      await writeFileAsync("../env.sh",
-                           [
-                             "#! /bin/bash",
-                             `export DISPLAY="${display}"`,
-                             `export "PATH=${atomPath}:${apmPath}:$PATH"`,
-                           ].join("\n"),
-                           {mode : "777"});
-    }
-    break;
-  }
   }
 }
 
